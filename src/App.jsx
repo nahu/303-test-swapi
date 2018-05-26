8a3167dea51d8b2d67e878703145e672f52ea644
Nahuel Hernández <jnahuelhernandez@gmail.com>
2018-05-26 13:06:48 -0400

    Added eslint (airbnb) deps and lint script

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+const App = props => (
+    <div className="App">
+        <header className="App-header">
+            <div className="row center yellow">
+                <div className="jumbotron">
+                    <h1>SWAPI</h1>
+                    <p className="lead">The Star Wars API</p>
+                </div>
+            </div>
+        </header>
+        <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+    </div>
+)
+
+
+export default App
